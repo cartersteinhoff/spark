569bd5df9cb39df9d2b95d621677874acb7cc85e
Carter Steinhoff <cartersteinhoff@gmail.com>
2018-08-21 16:07:41 -0700

    first push

--- apps/spark_app/models.py
+++ apps/spark_app/models.py
@@ -6,8 +6,10 @@ import datetime
 
 # User Model
 
+# class UserManager(models.manager):
 
-class User(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.CharField(max_length=40)
+
+# class User(models.Model):
+#     first_name = models.CharField(max_length=20)
+#     last_name = models.CharField(max_length=20)
+#     email = models.CharField(max_length=40)
